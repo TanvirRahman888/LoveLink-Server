5b2167e102ca89d22a29f58b48a417610bbbcb8b
Tanvir Module 1 <tanvirrahman8888@gmail.com>
2024-06-13 19:39:18 +0600

    biodata/:id Added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const app=express()
-const cors=require('cors')
+const app = express()
+const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion , ObjectId} = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ilfvfer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,10 +29,25 @@ async function run() {
     const BiodataCollection = client.db("LoveLink").collection("Biodata");
     const SuccessStoryCollection = client.db("LoveLink").collection("SuccessStory");
 
-    app.get('/biodata', async (req, res)=>{
-        const result =await BiodataCollection.find().toArray();
-        res.send(result)
+    app.get('/biodata', async (req, res) => {
+      const result = await BiodataCollection.find().toArray();
+      res.send(result)
     })
+    app.get('/biodata/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }; // Convert id to ObjectId
+      const result = await BiodataCollection.findOne(query);
+      res.send(result);
+    });
+
+    
+
+
+    app.get('/successstory', async (req, res) => {
+      const result = await SuccessStoryCollection.find().toArray();
+      res.send(result)
+    })
+    
 
 
     // Send a ping to confirm a successful connection
@@ -46,9 +61,9 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req,res)=>{
-    res.send("LoveLink Server is running")
+app.get('/', (req, res) => {
+  res.send("LoveLink Server is running")
 })
-app.listen(port, ()=>{
-    console.log("LoveLink is running on Port: ", port);
+app.listen(port, () => {
+  console.log("LoveLink is running on Port: ", port);
 })
